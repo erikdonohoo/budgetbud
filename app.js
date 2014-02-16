1b9d830744729c5824ddc9244665b2abeb97a082
Erik Donohoo <erikdonohoo@gmail.com>
2014-02-16 10:41:38 -0700

    Working on auth

--- app.js
+++ app.js
@@ -12,9 +12,30 @@ var app = module.exports = express();
 app.set('port', process.env.PORT || 3000);
 app.set('dbstring', process.env.MONGOHQ_URL);
 app.use(express.bodyParser());
+app.use(express.cookieParser('ed.budgetbud'));
+app.use(express.session());
 app.use(express.methodOverride());
 app.use(express.static('public'));
 app.use(app.router);
+
+// Middleware for session
+app.set('auth', function(req, res, next){
+
+	// Check session
+	if (!req.session || !req.sesssion.userid)
+		res.status(401).json({'error':'Need to Authenticate'});
+
+	var mongo = require('mongo');
+	mongo.MongoClient.connection(app.get('dbstring'), function(err, db){
+		var user = db.collection('user');
+		user.findOne({_id: new mongo.BSONPure.ObjectID(req.session.userid)}, function(err, item){
+			if (err)
+				res.status(403).json({'error':'Insufficient rights'});
+			else
+				next();
+		});
+	});
+});
 
 /**
  * Routes
--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,30 @@ angular.module('ed.budgetbud', ['ngRoute','ngResource','ngTouch','ngAnimate']).
 	return nav;
 }])
 
-.controller('NavCtrl', ['$scope','Nav', function($scope,Nav){
+.factory('User',['$q','$http', 
+	function($q, $http){
+
+	var user = {};
+
+	// Log In
+	user.logIn = function(credentials) {
+		var defer = $q.defer();
+		$http.post('/api/user/login', credentials).success(function(data){
+			user.setUser(data);
+			defer.resolve(data);
+		}).error(function(err){
+			defer.reject(err);
+		});
+		return defer.promise;
+	};
+
+	return user;
+
+}])
+
+.controller('NavCtrl', ['$scope','Nav','User', function($scope,Nav,User){
 	$scope.nav = Nav;
+	$scope.loggedIn = User.loggedIn
 }])
 
 .run(['$rootScope','$location','Nav',function($root,$loc){
