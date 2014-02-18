03e20efe0847b825b0149ea750980c7a5c2114e6
Erik Donohoo <donohooem@mtc.byu.edu>
2014-02-18 12:22:48 -0700

    Styling for budgets in place

--- public/js/controllers/budget.js
+++ public/js/controllers/budget.js
@@ -1,7 +1,20 @@
-angular.module('ed.budgetbud').controller('BudgetCtrl', ['$scope','Budget','$routeParams',
-	function($scope, Budget, $params){
+angular.module('ed.budgetbud').controller('BudgetCtrl', ['$scope','Budget','$routeParams','$timeout',
+	function($scope, Budget, $params,$timeout){
 
 	Budget.query($params).then(function(budgets){
 		$scope.budgets = budgets;
+		for (var i = $scope.budgets.length - 1; i >= 0; i--) {
+			var b = $scope.budgets[i];
+			b.spent = Math.floor(Math.random() * b.total);
+		}
+		$timeout(animateBudgets);
 	});
+
+	function animateBudgets() {
+		for (var i = 0; i < $scope.budgets.length; i++) {
+			var budget = $scope.budgets[i];
+			budget.style = {'width': Math.floor((1 - (budget.spent/budget.total)) * 100) + '%'};
+		}
+	}
+
 }]);
